9d0f64b7ac9ccc8c45840edc90d4ceb918f66d70
125Jasmine <125jasmine.hsueh@gmail.com>
2024-06-18 02:09:05 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,17 +1,23 @@
+package com;
+
 public class User {
     private String name = null;
-    private int gender = -1;
-    private int age = -1;
-    private double height = -1;
-    private double weight = -1;
-    private double bmi = -1;
-    private int dailyCalorieNeeds = -1;
-    private FoodRecord record = new FoodRecord();
+    private int gender = -1; 
+    private int age = -1; 
+    private double height = -1; 
+    private double weight = -1; 
+    private double bmi = -1; 
+    private int dailyCalorieNeeds = -1; 
+    public FoodRecord record = new FoodRecord();
     private Food sum = new Food();
 
     public void calculateBMI() {
         this.bmi = weight / ((height / 100) * (height / 100));
-    }
+    }   
+    
+    public double getBMI() {
+        return bmi;
+    }   
 
     public void calculateDailyCalorieNeeds() {
         if (bmi < 18.5) {
